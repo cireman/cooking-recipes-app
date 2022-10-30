aa2d6d9c44a4546ebbfd89f5eebb607d9f546ef7
cireman <ericpetrus@gmail.com>
2022-10-29 20:22:18 -0400

    add ingredients model

---
+++ src/models/ingredients.models.js
@@ -0,0 +1,36 @@
+const {DataTypes} = require('sequelize')
+const Types = require('./types.models')
+const db = require('../utils/database')
+
+const Ingredients = db.define('ingredients', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  typeId: {
+    type: DataType.INTEGER,
+    allowNull: false,
+    field: 'type_id',
+    reference: {
+      key: 'id',
+      model: Types
+    }
+  },
+  urlImg: {
+    type: DataTypes.STRING,
+    validate: {
+      usUrl: true
+    },
+    field: 'url_img'
+  }
+}, {
+  timestamps: false
+})
+
+module.exports = Ingredients
