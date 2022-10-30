8c71b7a1b2fa7ff36f16c76934b8ac46c7f6fa3a
cireman <ericpetrus@gmail.com>
2022-10-30 10:52:09 -0400

    add instructions model

---
+++ src/models/instructions.models.js
@@ -0,0 +1,29 @@
+const {DataTypes} = require('sequelize')
+const Recipes = require('./recipes.model')
+const db = require('../utils/database')
+
+const Instructions = db.define('instruction', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  step: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },recipeId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    field: recipe_id,
+    references: {
+      key: 'id',
+      model: Recipes
+    }
+  }
+})
+
+module.exports = Instructions
